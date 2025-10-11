4e1e4baa107de6cfe12cc32473f66fe8be7061ee
Alin Stoia <alin58236@gmail.com>
2025-10-11 20:45:58 +0300

    Changed init.sql

--- docker-databases/mssql/init.sql
+++ docker-databases/mssql/init.sql
@@ -1,16 +1,29 @@
 CREATE DATABASE univdb_mssql;
 GO
+
+CREATE LOGIN univuser WITH PASSWORD = 'Password123!';
+GO
+
 USE univdb_mssql;
 GO
 
-CREATE TABLE Teachers (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    name NVARCHAR(100) NOT NULL,
-    department NVARCHAR(100)
-);
+CREATE USER univuser FOR LOGIN univuser;
 GO
 
-INSERT INTO Teachers (name, department) VALUES
-('Prof. Smith', 'Mathematics'),
-('Prof. Johnson', 'Physics');
+ALTER ROLE db_owner ADD MEMBER univuser;
+GO
+
+USE univdb_mssql;
+GO
+
+CREATE TABLE Teachers (id INT IDENTITY(1,1) PRIMARY KEY,name NVARCHAR(100) NOT NULL,department NVARCHAR(100));
+GO
+
+USE univdb_mssql;
+GO
+
+INSERT INTO Teachers (name, department) VALUES ('Prof. Smith', 'Mathematics'),('Prof. Johnson', 'Physics');
+GO
+
+USE univdb_mssql;
 GO
